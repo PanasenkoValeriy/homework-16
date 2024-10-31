e53ae1e82bc9a03c9496370d9538b76c3094f35c
PanasenkoValeriy <124078834+PanasenkoValeriy@users.noreply.github.com>
2024-10-31 19:12:15 +0100

    upp

--- src/index.js
+++ src/index.js
@@ -5,20 +5,24 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 const input = document.getElementById('input');
-const list = document.getElementById('elementId');
+const list = document.getElementById('list');
 const country = document.getElementById('country');
-console.log('cdcd');
+
 input.addEventListener('input', debounce(onInputChange, 500));
 
 function onInputChange(e) {
   e.preventDefault();
   const inputValue = e.target.value.trim();
-  //   if (input === '') {
-  //     clearData();
-  //     return;
-  //   }
-  //   fetchCountries()
-  console.log(inputValue);
+  if (inputValue === '') {
+    clearData();
+    return;
+  }
+
+  fetchCountries(inputValue)
+    .then(fetchResponse)
+    .catch(error => {
+      notifyError(`Something went wrong - ${error}`);
+    });
 }
 
 function clearData() {
@@ -30,6 +34,24 @@ function renderCountry(countryInfo) {
   country.insertAdjacentHTML('beforeend', countryInfo);
 }
 
-function renderCountry(countryList) {
+function renderCountryList(countryList) {
   list.insertAdjacentHTML('beforeend', countryList);
+}
+
+function fetchResponse(countries) {
+  clearData();
+  if (countries.length > 1 && countries.length <= 10) {
+    const countryList = countries
+      .map(country => {
+        console.log(country);
+        return `
+          <li class="country__item">
+      <img class="country__item__img" src="${country.flags.svg}" alt="">
+      <p class="country__item__text">${country.name.common}</p>
+    </li> 
+          `;
+      })
+      .join('');
+    renderCountryList(countryList);
+  }
 }
